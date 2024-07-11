45a45fca1ebfba9c653c8b3179967dc8542fdc8d
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-07-11 11:18:57 +0200

    fix: use ipaddress instead of server name

--- 0-use_a_private_key.sh
+++ 0-use_a_private_key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the server address
-SERVER_ADDRESS="6225-web-01"
+SERVER_ADDRESS="172.17.255.255"
 
 # Use ssh to connect to the server using the private key
 ssh -i ~/.ssh/school ubuntu@$SERVER_ADDRESS
